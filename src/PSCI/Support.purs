2c8f1ac8866f6ce60a7206781edc6d6a13397c6a
Liam Goodacre <goodacre.liam@gmail.com>
2018-04-14 22:23:52 +0100

    Bump deps for compiler/0.12

--- src/PSCI/Support.purs
+++ src/PSCI/Support.purs
@@ -5,22 +5,19 @@ module PSCI.Support where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
+import Effect (Effect)
+import Effect.Console (logShow)
 
 -- | The `Eval` class captures those types which can be
 -- | evaluated in the REPL.
 -- |
--- | There are (possibly overlapping) instances provided for
--- | the `Eff` type constructor and any `Show`able types.
+-- | There are instances provided for the `Effect` type
+-- | constructor and any `Show`able types.
 class Eval a where
-  eval :: a -> Eff (console :: CONSOLE) Unit
+  eval :: a -> Effect Unit
 
-instance evalEff :: Eval a => Eval (Eff eff a) where
-  eval x = do
-    a <- unsafeCoerceEff x
-    eval a
+instance evalEffect :: Eval a => Eval (Effect a) where
+  eval x = x >>= eval
 else
 instance evalShow :: Show a => Eval a where
   eval = logShow
