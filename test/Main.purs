be724954fae5079c1c01c3b75eab2b6b9befdbfd
Harry Garrood <harry@garrood.me>
2018-05-04 22:53:53 +0100

    Update tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,15 +2,15 @@ module Test.Main where
 
 import Prelude (pure, unit, Unit)
 import PSCI.Support (eval)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Effect (Effect)
+import Effect.Console (logShow)
 
-egEvalShow :: Eff (console :: CONSOLE) Unit
+egEvalShow :: Effect Unit
 egEvalShow = eval 42
 
-egEvalEff :: Eff (console :: CONSOLE) Unit
+egEvalEff :: Effect Unit
 egEvalEff = eval (logShow 42)
 
-main :: Eff () Unit
+main :: Effect Unit
 main = pure unit
 
